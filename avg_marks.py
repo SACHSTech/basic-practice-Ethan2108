b8b9921f23085c15569f2c4dd4da731d9780c525
Ethan2108 <75390842+Ethan2108@users.noreply.github.com>
2020-12-02 16:41:59 -0500

    Update avg_marks.py

--- avg_marks.py
+++ avg_marks.py
@@ -1,1 +1,23 @@
+...
+------------------------------------------------------------
+Name: avg_marks.py
+Purpose: Compute the average of 4 marks 
 
+Author: Au.E
+
+Created: date in 02/12/2020
+_______________________________________________________________
+
+print("hello")
+# get 4 marks from the UserWarning
+mark1 = float(input("Enter mark 1"))
+mark2 = float(input("Enter mark 2"))
+mark3 = float(input("Enter mark 3"))
+mark4 = float(input("Enter mark 4"))
+
+# compute the average of the 4 marks
+marks_avg = (mark1 + mark2 + mark3 + mark4)/4
+
+# output the average
+print("The average of the 4 marks is" + str(marks_avg))
+=======
