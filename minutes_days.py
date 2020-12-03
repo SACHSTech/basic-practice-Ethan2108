dc1f44f2c641e9fbd38f3897ff8eebacfad14434
Ethan2108 <75390842+Ethan2108@users.noreply.github.com>
2020-12-03 17:00:42 -0500

    Delete minutes_days.py

--- minutes_days.py
+++
@@ -1,4 +0,0 @@
-minutes = float(input("Insert number of minutes "))
-days = (minutes / 1440 // 1.000)
-hours = (minutes // 60)
-print (str(days) + " days, " + str(hours) + " hours and " + str(minutes) + " minutes" )
