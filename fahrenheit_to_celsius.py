8c6144d59fc7f778578ddec4c5f67cd7139502a2
Ethan2108 <75390842+Ethan2108@users.noreply.github.com>
2020-12-03 14:17:20 -0500

    Create fahrenheit_to_celsius.py

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,10 @@
+print ("what is the temperature in fahrenheit?")
+temperature = float(input("enter the temperature in fahrenheit"))
+
+fahrenheit = float(input("enter the temperature in fahrenheit"))
+
+# compute the temperature in celsius
+celsius = 5/9(fahrenheit - 32)
+
+# output the temperature in celsius
+print ("The temperature in celsius is" + str(celsius))
