f1fe2e1569553046ae29bd989ccf0bdfc59059f4
Ethan2108 <75390842+Ethan2108@users.noreply.github.com>
2020-12-03 17:01:31 -0500

    Delete f_to_c.py

--- f_to_c.py
+++
@@ -1,5 +0,0 @@
-#Asks user for the amount of degrees fahrenheit
-f = float(input("Degrees fahrenheit: "))
-
-#Tells user the amount of degrees celcius after conversion
-print(f , "degrees fahrenheit equals", float((f - 32) * (5/9) ), "degrees celcius.")
